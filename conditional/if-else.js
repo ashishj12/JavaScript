beb84b7b66c91979286c473557fd0fd5c8db94ec
ashishj12 <jangidashish38@gmail.com>
2024-04-29 21:21:11 +0530

    conditional statements in js

---
+++ conditional/if-else.js
@@ -0,0 +1,10 @@
+//example 1
+let myAge = 15;
+
+if(myAge >= 18){
+    console.log("I can Drive");
+}else{
+    console.log("Cannot Drive");
+}
+
+
---
+++ conditional/if.js
@@ -0,0 +1,22 @@
+//example 1
+let age = 10;
+
+if (age > 18)
+    console.log("You can Vote!");
+else
+    console.log("You can not vote")
+
+//example 2
+let speed = 87;
+
+if(speed > 90){
+    console.log("OverSpeeding");
+}
+
+if(speed > 50 && speed <=90){
+    console.log("Normal");
+}
+
+if(speed <= 50){
+    console.log("Slow");
+}
