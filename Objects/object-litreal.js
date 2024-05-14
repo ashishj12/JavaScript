fce2999bde1f48276f3d3f9c4948beacaeac884d
ashishj12 <jangidashish38@gmail.com>
2024-05-14 22:23:51 +0530

    objects in js

---
+++ Objects/object-litreal.js
@@ -0,0 +1,12 @@
+let person = {
+    firstname: 'Ashish',
+    lastname: 'Kumar',
+    age: 21,
+
+    hobbies : ['Coding', 'Gaming', 'Animation'],
+
+    message: function () {
+        console.log("Hi,my name is " + this.firstname);
+    }
+
+};
