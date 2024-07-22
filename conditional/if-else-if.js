6c791c232ab3934b2f86b30159e5f3de8936f326
ashishj12 <jangidashish38@gmail.com>
2024-07-22 15:58:05 +0530

    conditional statements

---
+++ conditional/if-else-if.js
@@ -0,0 +1,10 @@
+let weight = 65;
+
+if (weight > 70) {
+    console.log("You are Overweight");
+} else if (weight > 50 && weight <= 70) {
+    console.log("You are Fit");
+} else {
+    console.log("You are Underweight");
+}
+
