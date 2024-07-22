26b5fe3af51b238a139b7776e1948169968dffd5
ashishj12 <jangidashish38@gmail.com>
2024-07-22 15:48:07 +0530

    DataTypes

---
+++ datatypes/math_object.js
@@ -0,0 +1,9 @@
+const PI = Math.PI
+console.log(PI) // 3.141592653589793
+console.log(Math.round(PI)) // 3; to round values to the nearest number
+console.log(Math.round(9.81)) // 10
+console.log(Math.floor(PI)) // 3; rounding down
+console.log(Math.ceil(PI)) // 4; rounding up
+
+const randNum = Math.random() // creates random number between 0 to 0.999999
+console.log(randNum)
---
+++ datatypes/string_concatenation.js
@@ -0,0 +1,11 @@
+// Declaring different variables of different data types
+let space = ' '
+let firstName = 'Ashish'
+let lastName = 'Kumar'
+let language = 'JavaScript'
+
+// Concatenating using addition operator
+let fullName = firstName + space + lastName // concatenation, merging two string together.
+console.log(fullName)
+
+ console.log("")
