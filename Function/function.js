4d21eefea1223ffecf06f704544a76bef9210047
ashishj12 <jangidashish38@gmail.com>
2024-07-23 06:45:37 +0530

    Function  and Arrow Function

---
+++ Function/function.js
@@ -0,0 +1,44 @@
+// function myFunc(theObject) {
+//     theObject.make = "Book";
+// }
+
+// const myBook = {
+//     title: "JS",
+//     author: "ABC",
+//     year: 2000,
+// };
+
+// console.log(myBook.make);
+// myFunc(myBook);
+// console.log(myBook.make);
+
+
+// const square = function (number) {
+//     return number * number;
+// };
+
+// console.log(square(4)); // 16
+
+
+// const factorial = function fac(n) {
+//     return n < 2 ? 1 : n * fac(n - 1);
+// };
+
+// console.log(factorial(3)); // 6
+
+
+
+// function map(f, a) {
+//     const result = new Array(a.length);
+//     for (let i = 0; i < a.length; i++) {
+//         result[i] = f(a[i]);
+//     }
+//     return result;
+// }
+
+// const cube = function (x) {
+//     return x * x * x;
+// };
+
+// const numbers = [0, 1, 2, 5, 10];
+// console.log(map(cube, numbers)); // [0, 1, 8, 125, 1000]
